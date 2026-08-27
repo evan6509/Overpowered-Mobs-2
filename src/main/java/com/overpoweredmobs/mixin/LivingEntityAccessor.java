a5bd8a83bf2671c64f00bc0564d906275707d52e
Evan Chubbuck <69433812+evan6509@users.noreply.github.com>
2026-08-26 19:42:40 -0500

    Fix LivingEntity accessor for Minecraft 26.2

--- src/main/java/com/overpoweredmobs/mixin/LivingEntityAccessor.java
+++ src/main/java/com/overpoweredmobs/mixin/LivingEntityAccessor.java
@@ -7,6 +7,6 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(LivingEntity.class)
 public interface LivingEntityAccessor {
 
-    @Invoker("setSharedFlag")
+    @Invoker("setLivingEntityFlag")
     void opmSetSharedFlag(int flag, boolean value);
 }
